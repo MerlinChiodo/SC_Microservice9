c23a14ef84b45ac2d7c776a885be46978305e543
jt128 <82189306+jt128@users.noreply.github.com>
2022-04-19 23:01:02 +0200

    Add typescript support for nextjs
    - change *.js files to *.tsx files
    - add logo + favicon
    - removed unused files

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Amt für Integration</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
