b4d84fda4f295f057e6d79b59426221463c2c50a
jt128 <82189306+jt128@users.noreply.github.com>
2022-04-29 15:07:08 +0200

    UPDATE tests for public api
    -  add mock data for valid qr code ( because prisma client does not work in cypress test runner)
    -  add negative test cases
    -  add tests for method type

--- cypress/integration/api/refugeeStatus.js
+++ cypress/integration/api/refugeeStatus.js
@@ -1,23 +1,110 @@
 // API - Refugee Status Tests
 describe('Refugee Status API', () => {
-  it('verify request return JSON', () => {
-    cy.request('http://localhost:3000/api/refugee/asl999klf3jkla')
-      .its('headers')
-      .its('content-type')
-      .should('include', 'application/json');
+  describe('Test Valid QR Code', () => {
+    beforeEach(function () {
+      cy.fixture('refugee').then((refugee) => {
+        this.refugee = refugee;
+      });
+    });
+
+    it('verify request return JSON', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.refugee.qr_code}`)
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json');
+    });
+
+    it('verfiy the response, correct status code', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.refugee.qr_code}`)
+        .its('status')
+        .should('eq', 200);
+    });
+
+    it('verify the response, contains item status', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.refugee.qr_code}`)
+        .its('body')
+        .should('have.property', 'status');
+    });
+
+    it('verify the response, status is true', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.refugee.qr_code}`)
+        .its('body.status')
+        .should('be.a', 'boolean')
+        .should('eq', true);
+    });
   });
 
-  it('verfiy the request returns the correct status code', () => {
-    cy.request('http://localhost:3000/api/refugee/asl999klf3jkla').its('status').should('eq', 200);
+  describe('Test Invalid QR Code', () => {
+    beforeEach(function () {
+      this.invalidQRCode = 'invalidQRCode9909020abcd';
+    });
+
+    it('verify request return JSON', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.invalidQRCode}`)
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json');
+    });
+
+    it('verfiy the response, correct status code', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.invalidQRCode}`)
+        .its('status')
+        .should('eq', 200);
+    });
+
+    it('verify the response, contains item status', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.invalidQRCode}`)
+        .its('body')
+        .should('have.property', 'status');
+    });
+
+    it('verify the response, status is false', function () {
+      cy.request(`http://localhost:3000/api/refugee/${this.invalidQRCode}`)
+        .its('body.status')
+        .should('be.a', 'boolean')
+        .should('eq', false);
+    });
   });
 
-  it('verify the request contains  item status', () => {
-    cy.request('http://localhost:3000/api/refugee/asl999klf3jkla')
-      .its('body')
-      .should('have.property', 'status');
-  });
+  describe('Verify Method Types', () => {
 
-  it('verify the request item is boolean', () => {
-    cy.request('http://localhost:3000/api/refugee/asl999klf3jkla').its('body.status').should('be.a', 'boolean');
+    it('GET Method', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:3000/api/refugee/test',
+      })
+        .its('status')
+        .should('eq', 200);
+    });
+
+    it('POST Method', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3000/api/refugee/test',
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('eq', 405);
+    });
+
+    it('DELETE Method', () => {
+      cy.request({
+        method: 'DELETE',
+        url: 'http://localhost:3000/api/refugee/test',
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('eq', 405);
+    });
+
+    it('PUT Method', () => {
+      cy.request({
+        method: 'PUT',
+        url: 'http://localhost:3000/api/refugee/test',
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should('eq', 405);
+    });
   });
 });
