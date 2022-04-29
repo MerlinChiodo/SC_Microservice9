b3d2d3c3ee37aec02d6ce280cebceac99b5083ff
jt128 <82189306+jt128@users.noreply.github.com>
2022-04-29 14:58:19 +0200

    UPDATE public api
    - Change naming to improve code quality
    - Check for correct datatype before accessing data

--- pages/api/refugee/[qr_code].ts
+++ pages/api/refugee/[qr_code].ts
@@ -3,14 +3,19 @@ import { prisma } from '../../../lib/prisma';
 
 export default async function refugeeHandler(req: NextApiRequest, res: NextApiResponse) {
   const {
-    query: { id },
+    query: { qr_code },
     method,
   } = req;
+
+  if (typeof qr_code !== 'string') {
+    res.status(400).json({ statusCode: 400, message: 'Invalid QR code' });
+    return;
+  }
 
   switch (method) {
     case 'GET':
       const result = await prisma.refugee.findFirst({
-        where: { qr_code: typeof id === 'string' ? id : undefined },
+        where: { qr_code: qr_code },
       });
       if (!result) {
         res.status(200).json({ status: false });
