5861ec190361d013f956fbc63010c7df770befc2
jt128 <82189306+jt128@users.noreply.github.com>
2022-05-04 01:35:19 +0200

    UPDATE event "Register New Refugee Family"
    - one api call
    - handles creating refugee
    - handles events
    - generating qr_code, assignHousing only interfaces for MVP without logic, marked with TODO\
    - update tests

--- pages/api/private/refugee/jsonSchema.ts
+++ pages/api/private/refugee/jsonSchema.ts
@@ -11,6 +11,11 @@ export interface RegisterRefugee {
   document: Buffer;
 }
 
+export interface RegisterRefugeeFamily {
+  parents: RegisterRefugee[];
+  children: RegisterRefugee[];
+}
+
 interface RegisterRefugeeRabbitMQData {
   firstname: string;
   lastname: string;
@@ -23,6 +28,14 @@ export interface RegisterRefugeeRabbitMQ {
   event_name: string;
   service_name: string;
   refugee: RegisterRefugeeRabbitMQData;
+}
+
+export interface RegisterRefugeeFamilyRabbitMQ {
+  event_id: number;
+  event_name: string;
+  service_name: string;
+  parents: RegisterRefugeeRabbitMQData[];
+  children: RegisterRefugeeRabbitMQData[];
 }
 
 export const RegisterRefugeeSchema: JSONSchemaType<RegisterRefugee> = {
@@ -113,3 +126,168 @@ export const RegisterRefugeeRabbitMQSchema: JSONSchemaType<RegisterRefugeeRabbit
   additionalProperties: false,
 };
 
+export const RegisterRefugeeFamilySchema: JSONSchemaType<RegisterRefugeeFamily> = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Send New Family to Buergerbuero',
+  description: 'Data for a new Family',
+  type: 'object',
+  properties: {
+    parents: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          firstname: {
+            type: 'string',
+            minLength: 1,
+          },
+          lastname: {
+            type: 'string',
+            minLength: 1,
+          },
+          date_of_birth: {
+            type: 'string',
+            format: 'date-time',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+          },
+          phone: {
+            type: 'string',
+            minLength: 1,
+          },
+          nationality: {
+            type: 'string',
+            minLength: 1,
+          },
+          language: {
+            type: 'string',
+            minLength: 1,
+          },
+          document: {
+            type: 'object',
+            required: [],
+          },
+        },
+        required: ['firstname', 'lastname', 'date_of_birth', 'email'],
+      },
+    },
+    children: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          firstname: {
+            type: 'string',
+            minLength: 1,
+          },
+          lastname: {
+            type: 'string',
+            minLength: 1,
+          },
+          date_of_birth: {
+            type: 'string',
+            format: 'date-time',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+          },
+          phone: {
+            type: 'string',
+            minLength: 1,
+          },
+          nationality: {
+            type: 'string',
+            minLength: 1,
+          },
+          language: {
+            type: 'string',
+            minLength: 1,
+          },
+          document: {
+            type: 'object',
+            required: [],
+          },
+        },
+        required: ['firstname', 'lastname', 'date_of_birth', 'email'],
+      },
+    },
+  },
+  required: ['parents', 'children'],
+  additionalProperties: false,
+};
+
+export const RefugeeFamilyRabbitMQSchema: JSONSchemaType<RegisterRefugeeFamilyRabbitMQ> = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Send New Refugee Family to Buergerbuero via RabbitMQ',
+  description: 'Event data',
+  type: 'object',
+  properties: {
+    event_id: {
+      type: 'integer',
+      const: 9001,
+    },
+    event_name: {
+      type: 'string',
+      const: 'Register New Refugee Family',
+    },
+    service_name: {
+      type: 'string',
+      const: 'integration',
+    },
+    parents: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          firstname: {
+            type: 'string',
+            minLength: 1,
+          },
+          lastname: {
+            type: 'string',
+            minLength: 1,
+          },
+          date_of_birth: {
+            type: 'string',
+            format: 'date',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+          },
+        },
+        required: ['firstname', 'lastname', 'date_of_birth', 'email'],
+      },
+    },
+    children: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          firstname: {
+            type: 'string',
+            minLength: 1,
+          },
+          lastname: {
+            type: 'string',
+            minLength: 1,
+          },
+          date_of_birth: {
+            type: 'string',
+            format: 'date',
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+          },
+        },
+        required: ['firstname', 'lastname', 'date_of_birth', 'email'],
+      },
+    },
+  },
+  required: ['event_id', 'event_name', 'service_name', 'parents', 'children'],
+  additionalProperties: false,
+};
--- pages/api/private/refugee/registerHelper.ts
+++ pages/api/private/refugee/registerHelper.ts
@@ -1,10 +1,46 @@
+import { prisma } from '../../../../lib/prisma';
+import { registerRefugeeFamilyEventHandler } from './eventFamily';
+import { RegisterRefugeeFamily } from './jsonSchema';
+
 export function generateQRCode() {
   // TODO
-  return Math.random().toString(36).substr(2, 10) //random string
-
+  return Math.random().toString(36).substr(2, 10); //random string
 }
 
 export function assignHousing() {
   // TODO
   return 1;
+}
+
+export function registerFamily(rf: RegisterRefugeeFamily) {
+  const event = {
+    event_id: 9001,
+    event_name: 'Register New Refugee Family',
+    service_name: 'integration',
+    parents: rf.parents,
+    children: rf.children,
+  };
+  const parentsList: any = [];
+  const childrenList: any = [];
+  const housing_id = assignHousing();
+  rf.parents.forEach((p, i) => {
+    const qr_code = generateQRCode();
+    const refugee = prisma.refugee
+      .create({
+        data: { ...p, qr_code: qr_code, housing_id: housing_id },
+      })
+      .catch((err) => new Error(err));
+    parentsList.push(refugee);
+  });
+  rf.children.forEach((c, i) => {
+    const qr_code = generateQRCode();
+    const child = prisma.refugee
+      .create({
+        data: { ...c, qr_code: qr_code, housing_id: housing_id },
+      })
+      .catch((err) => new Error(err));
+    childrenList.push(child);
+  });
+  registerRefugeeFamilyEventHandler(event).catch((err) => new Error(err));
+  return { parents: parentsList, children: childrenList };
 }
