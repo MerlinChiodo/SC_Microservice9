2a1ec85cc9e2ecc41747a5ac981ea290fd2f6c0f
jt128 <82189306+jt128@users.noreply.github.com>
2022-05-21 15:32:27 +0200

    ADD helpers
    - helper function for fetching
    - helper function for stripe
    - interfaces

---
+++ util/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface Employee {
+  firstname: string;
+  lastname: string;
+  room: string;
+  phone: string;
+  email: string;
+  picture: string;
+}
+
+export interface User {
+  firstname: string;
+  lastname: string;
+  email: string;
+}
