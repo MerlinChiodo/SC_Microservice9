1eaefb723def503ba8becb0d7a39f4c0021bc37a
jt128 <82189306+jt128@users.noreply.github.com>
2022-05-21 15:38:08 +0200

    ADD auth context provider
    - with mock function, since SmartAuth is not ready to use

---
+++ context/auth/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext, useEffect, useMemo } from 'react';
+import type { User } from 'util/interfaces';
+
+interface AuthContext {
+  user: User | null;
+  logout: Function;
+  login: Function;
+}
+
+const AuthContext = React.createContext<AuthContext>({user: null, logout: () => {}, login: () => {}});
+
+export const useAuth = () => useContext(AuthContext);
+
+
+export function AuthProvider({ children }: { children: React.ReactNode }): JSX.Element {
+  const [user, setUser] = useState<User | null>(null);
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  const login = () => {
+    setUser({ firstname: 'Hans', lastname: 'Bauer', email: 'hans@bauer.com' });
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    //check token & get user infos
+    if (token) {
+      login()
+    } else {
+      logout()
+    }
+  }, [setUser]);
+
+  const state = useMemo(
+    () => ({
+      user,
+      login,
+      logout,
+    }),
+    [user]
+  );
+
+return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
+}
