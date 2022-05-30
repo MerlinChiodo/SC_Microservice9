e8cf4b48f45e4b78c09f3c78e96c48d36c2492c1
MerlinChiodo <60719496+MerlinChiodo@users.noreply.github.com>
2022-05-30 17:05:11 +0200

    Update index.api.ts

--- pages/api/private/housing/index.api.ts
+++ pages/api/private/housing/index.api.ts
@@ -11,7 +11,7 @@ export default async function housingHandler(req: NextApiRequest, res: NextApiRe
   addFormats(ajv);
 
   req.cookies.token = 'test';
-  if (!auth(req.cookies.token)) {
+  if (!await auth(req.cookies.token)) {
     res.status(403).end(`No Authorization`);
     return;
   }
