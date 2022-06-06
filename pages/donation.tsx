a2ce073a202b0e569933a3788eb34cb94c4526be
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-06 15:50:56 +0200

    UPDATE export variables in util/stripe

--- pages/donation.tsx
+++ pages/donation.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Blockquote, Center, Loader, Title, useMantineTheme } from '@mantine/core';
 import Layout from 'components/layout';
 import getStripe from 'lib/stripe';
 import { PaymentIntent } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { fetchPostJSON } from 'util/api/fetch';
 import DonationForm from 'components/donationForm';
-import { Center, Loader } from '@mantine/core';
-
-const MAX_AMOUNT = 5000;
-const AMOUNT_STEP = 5;
+import { MAX_AMOUNT, AMOUNT_STEP } from 'util/stripe';
 
 export default function Donation() {
   const [paymentIntent, setPaymentIntent] = useState<PaymentIntent | null>(null);
