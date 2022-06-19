0ea69dd17d8a8848de03757d8829d2eaf5e04b6e
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-19 09:47:14 +0200

    ADD smart auth url

--- util/server.ts
+++ util/server.ts
@@ -2,3 +2,5 @@ export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? 'http://vps2290194.fastwebserver.de:9790'
     : 'http://localhost:3000';
+
+export const AUTH_URL = 'http://auth.smartcityproject.net:8080';
