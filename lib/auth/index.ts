4f62646647a19ee8509e5441b44f4029c35839d3
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-19 09:52:34 +0200

    UPDATE verify auth for api calls

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,4 +1,13 @@
+import { AUTH_URL } from 'util/server';
+import { fetchPostURLEncoded } from 'util/api/fetch';
+import { isNotEmpty } from 'util/api/util';
+
 export default async function auth(token: string) {
-  // fetch SmartAuth
-  return true
+  if (isNotEmpty(process.env.NEXT_PUBLIC_E2E_TEST)) return true;
+  try {
+    await fetchPostURLEncoded(AUTH_URL + '/verify', { code: token });
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
