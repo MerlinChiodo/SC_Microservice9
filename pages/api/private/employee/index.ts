3c6c5cc95eb6330c644a2503644302d3c2311c63
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-19 09:51:04 +0200

    REMOVE auth query
    - routes should work without auth

--- pages/api/private/employee/index.ts
+++ pages/api/private/employee/index.ts
@@ -1,10 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from 'lib/prisma';
-import { methodNotAllowed, notAuthenticated } from 'util/api/util';
-import auth from 'lib/auth';
+import { methodNotAllowed } from 'util/api/util';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!(await auth(req.cookies.token))) return notAuthenticated(res);
   if (req.method === 'GET') {
     const employees = await prisma.employee.findMany();
     return res.status(200).json(employees);
