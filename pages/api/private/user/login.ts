62f7901f833d21677dd9e096fe99952d6f3a687b
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-20 14:53:53 +0200

    UPDATE login

--- pages/api/private/user/login.ts
+++ pages/api/private/user/login.ts
@@ -6,23 +6,21 @@ import cookie from 'cookie';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { username, password } = req.body;
-  try {
-    const data = await fetchPostURLEncoded(AUTH_URL + '/login', {
-      username: username,
-      password: password,
-    });
-    const user = { ...data.info, username: data.username, citizen_id: data.citizen_id };
-    res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('token', data.user_session_token, {
-        httpOnly: true,
-        maxAge: 60 * 60,
-        sameSite: 'strict',
-        path: '/',
-      })
-    );
-    return res.status(200).json(user);
-  } catch (e) {
-    return notAuthenticated(res);
-  }
+  const response = await fetchPostURLEncoded(AUTH_URL + '/login', {
+    username: username,
+    password: password,
+  });
+  if (response.status != 200) return notAuthenticated(res);
+  const data = await response.json();
+  const user = { ...data.info, username: data.username, citizen_id: data.citizen_id };
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('token', data.user_session_token, {
+      httpOnly: true,
+      maxAge: 60 * 60,
+      sameSite: 'strict',
+      path: '/',
+    })
+  );
+  return res.status(200).json(user);
 }
