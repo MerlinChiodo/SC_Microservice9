afd7b0166331a29fe8728bf394893232408d3fed
jt128 <82189306+jt128@users.noreply.github.com>
2022-06-27 19:15:08 +0200

    UPDATE auth url + base url

--- util/server.ts
+++ util/server.ts
@@ -1,9 +1,9 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://vps2290194.fastwebserver.de:9790'
+    ? 'http://www.supersmartcity.de:9790'
     : 'http://localhost:3000';
 
-export const AUTH_URL = 'http://auth.smartcityproject.net:8080';
+export const AUTH_URL = 'http://www.supersmartcity.de:9760';
 
 export const PORTAL_URL = 'http://supersmartcity.de';
 
